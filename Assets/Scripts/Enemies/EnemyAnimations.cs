ed6e7346a7ba9a834f7d18a49ca7c12db48735fe
Cường Trần <112959652+cuongtrann@users.noreply.github.com>
2023-07-03 09:53:14 +0700

    Enemy hurt and die Reset new wave when remain enemies = 0 (Die or in the end of waypoint)

---
+++ Assets/Scripts/Enemies/EnemyAnimations.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimations : MonoBehaviour
+{
+    private Animator _animator;
+    private EnemyManager _enemy;
+    private EnemyHealth _enemyHealth;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _enemy = GetComponent<EnemyManager>();
+        _enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    private void PlayHurtAnimation()
+    {
+        _animator.SetTrigger("Hurt");
+    }
+
+    private void PlayDieAnimation()
+    {
+        _animator.SetTrigger("Die");
+    }
+
+    private float GetCurrentAnimationLength()
+    {
+        float animationLength = _animator.GetCurrentAnimatorStateInfo(0).length;
+        return animationLength;
+    }
+
+    private IEnumerator PlayHurt()
+    {
+        _enemy.StopMovement();
+        PlayHurtAnimation();
+        yield return new WaitForSeconds(GetCurrentAnimationLength() + 0.3f);
+        _enemy.ResumeMovement();
+    }
+
+    private IEnumerator PlayDead()
+    {
+        _enemy.StopMovement();
+        PlayDieAnimation();
+        yield return new WaitForSeconds(GetCurrentAnimationLength() + 0.3f);
+        _enemy.ResumeMovement();
+        _enemyHealth.ResetHealth();
+        ObjectPooler.ReturnToPool(_enemy.gameObject);
+    }
+
+    private void EnemyHit(EnemyManager enemy)
+    {
+        if (_enemy == enemy)
+        {
+            StartCoroutine(PlayHurt());
+        }
+    }
+
+    private void EnemyDead(EnemyManager enemy)
+    {
+        if (_enemy == enemy)
+        {
+            StartCoroutine(PlayDead());
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        EnemyHealth.OnEnemyHit += EnemyHit;
+        EnemyHealth.OnEnemyKilled += EnemyDead;
+    }
+
+
+    private void OnDisable()
+    {
+        EnemyHealth.OnEnemyHit -= EnemyHit;
+        EnemyHealth.OnEnemyKilled -= EnemyDead;
+    }
+}
